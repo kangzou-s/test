f82d55b4e31c813a76a2ee690d90956a59639f0e
清欢 <qinghuan@qinghuandeMacBook-Pro.local>
2021-03-19 19:34:44 +0100

    add new feature

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ surnames = ['Sugaro', 'Cuppo', 'Creamoff', 'Bru', 'Lipton']
 # in format "Name Surname". At the moment, for some reason, they are
 # printed as "NameSurname" without a space, which is wrong.
 for i in range(len(names)):
-    print(names[i] + surnames[i])
+    print(names[i] + " "+surnames[i])
 
 
 
@@ -20,3 +20,7 @@ for i in range(len(names)):
 # Commit this within branch 'feature' and then merge to 'master'
 
 # Your code goes here:
+for i in range(len(names)):
+	for j in range (len(surnames)):
+		print(names[i] + " "+surnames[j])
+
